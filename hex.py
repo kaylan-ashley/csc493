18ab60782db740501dabce0ba2c5d91ace35428d
smithk73 <k.arnoldt.smith@mail.utoronto.ca>
2019-02-11 06:35:47 -0500

    Modify compute_paths() to avoid as many cells as necessary

--- hex.py
+++ hex.py
@@ -16,36 +16,39 @@ import time
 RED = True
 BLUE = False
 
-parser = argparse.ArgumentParser()
-parser.add_argument('rows', type=int, help='number of rows in the grid')
-parser.add_argument('cols', type=int, help='number of columns in the grid')
-parser.add_argument('cell_to_avoid', type=str, nargs='?', default=None,
-                    help='coordinates of cell to avoid')
-parser.add_argument('--interactive', action='store_true', 
-                    help='enable interactive mode')
-parser.add_argument('--visual', action='store_true',
-                    help='enable visual mode')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('rows', type=int, help='number of rows in the grid')
+    parser.add_argument('cols', type=int, help='number of columns in the grid')
+    parser.add_argument('cell_to_avoid', type=str, nargs='?', default=None,
+                        help='coordinates of cell to avoid')
+    parser.add_argument('--interactive', action='store_true', 
+                        help='enable interactive mode')
+    parser.add_argument('--visual', action='store_true',
+                        help='enable visual mode')
+    parser.add_argument('--puzzle', default='1',
+                        help='enable puzzle mode')
 
-args = parser.parse_args()
-rows = args.rows
-cols = args.cols
-if args.cell_to_avoid:
-    x, y = (int(c.strip("(), []")) for c in args.cell_to_avoid.split(','))
-    if 0 <= x < cols and 0 <= y < rows:
-        cell_to_avoid = (x, y)
-    else:
-        cell_to_avoid = None
-else:
-    cell_to_avoid = None
-interactive = args.interactive
-visual = args.visual
+    args = parser.parse_args()
+    rows = args.rows
+    cols = args.cols
+    cell_to_avoid = []
+    if args.cell_to_avoid:
+        x, y = (int(c.strip("(), []")) for c in args.cell_to_avoid.split(','))
+        if 0 <= x < cols and 0 <= y < rows:
+            cell_to_avoid.append((x, y))
+    interactive = args.interactive
+    visual = args.visual
+    if args.puzzle:
+        puzzle_number = int(args.puzzle.strip())
+        print('you asked for puzzle number ' + str(puzzle_number)) #REMOVE
 
-possible_moves = [(0, 1), (1, 1), (1, 0), (0, -1), (-1, -1), (-1, 0)]
+    possible_moves = [(0, 1), (1, 1), (1, 0), (0, -1), (-1, -1), (-1, 0)]
 
-all_lengths_red = {}
-lengths_avoiding_cell_red = {}
-all_lengths_blue = {}
-lengths_avoiding_cell_blue = {}
+    all_lengths_red = {}
+    lengths_avoiding_cell_red = {}
+    all_lengths_blue = {}
+    lengths_avoiding_cell_blue = {}
 
 
 class Path:
@@ -86,9 +89,9 @@ def print_result(all_lengths, lengths_avoiding_cell):
 
     if cell_to_avoid:
         avoid_lst = sorted(lengths_avoiding_cell.items(), key=lambda x: (x[0]))
-        print("\n# of minimal paths avoiding cell " + str(cell_to_avoid) \
+        print("\n# of minimal paths avoiding cell(s) " + str(cell_to_avoid) \
               + ": " + str(num_paths_avoiding_cell))
-        print("length distribution of minimal paths that avoid cell " \
+        print("length distribution of minimal paths that avoid cell(s) " \
               + str(cell_to_avoid) + ":")
         print(avoid_lst)
 
@@ -169,7 +172,7 @@ def compute_paths(rows, cols, cell_to_avoid, player):
 
         lengths_avoiding_cell[1] = cols
         if cell_to_avoid:
-            lengths_avoiding_cell[1] -= 1
+            lengths_avoiding_cell[1] -= len(cell_to_avoid)
         num_paths_avoiding_cell = lengths_avoiding_cell[1]
 
         # for consistency, remove key if value is 0
@@ -203,9 +206,16 @@ def compute_paths(rows, cols, cell_to_avoid, player):
                     # if path does not include cell to be avoided, add length
                     # to distribution of lengths of paths avoiding cell
                     if cell_to_avoid:
-                        if next_move != cell_to_avoid and p.last != cell_to_avoid:
-                            if cell_to_avoid not in p.all_but_last:
-                                add_length(path_length, lengths_avoiding_cell)
+                        ok_to_add = True
+                        for i in range(len(cell_to_avoid)):
+                            if next_move == cell_to_avoid[i] or p.last == cell_to_avoid[i]:
+                                ok_to_add = False
+                                break
+                            if cell_to_avoid[i] in p.all_but_last:
+                                ok_to_add = False
+                                break
+                        if ok_to_add:
+                            add_length(path_length, lengths_avoiding_cell)
 
                     # add path to list of paths
                     new_path = p.all_but_last.copy()
@@ -227,6 +237,7 @@ def compute_paths(rows, cols, cell_to_avoid, player):
     else:
         all_lengths_blue = all_lengths
         lengths_avoiding_cell_blue = lengths_avoiding_cell
+
     return paths
 
 
@@ -238,172 +249,172 @@ def get_open_tiles(rows, cols, red_moves, blue_moves):
                 open_tiles.append((x, y))
     return open_tiles
 
+if __name__ == "__main__":
+    if (interactive):
+        game_over = 0 # initialize game loop
+        turn = RED # red goes first
+        red_moves = set() 
+        blue_moves = set()
 
-if (interactive):
-    game_over = 0 # initialize game loop
-    turn = RED # red goes first
-    red_moves = set() 
-    blue_moves = set()
+        # Compute sets of minimal winning paths for Red and Blue
+        red_paths = compute_paths(rows, cols, None, RED)
+        if rows == cols:
+            blue_paths = red_paths.copy() # saves time, especially on larger grids
+        else:
+            blue_paths = compute_paths(cols, rows, None, BLUE) 
 
-    # Compute sets of minimal winning paths for Red and Blue
-    red_paths = compute_paths(rows, cols, None, RED)
-    if rows == cols:
-        blue_paths = red_paths.copy() # saves time, especially on larger grids
-    else:
-        blue_paths = compute_paths(cols, rows, None, BLUE) 
+        # We computed the set of Blue paths by rotating the board 90 degrees,
+        # so we need to swap the coordinates of the tiles in those paths.
+        for i in range(0, len(blue_paths)):
+            path = set()
+            for tile in blue_paths[i]:
+                path.add((tile[1], tile[0]))
+            blue_paths[i] = path
 
-    # We computed the set of Blue paths by rotating the board 90 degrees,
-    # so we need to swap the coordinates of the tiles in those paths.
-    for i in range(0, len(blue_paths)):
-        path = set()
-        for tile in blue_paths[i]:
-            path.add((tile[1], tile[0]))
-        blue_paths[i] = path
+        while not game_over:
+            
+            # print game state and get user input
+            while True:             
+                if visual:
+                    print_grid(rows, cols, red_moves, blue_moves)
+                else:
+                    lst = sorted(all_lengths_red.items(), key=lambda x: (x[0]))
+                    print("\nlength distribution of minimal winning paths remaining (Red): ")
+                    print(lst)
+                    
+                    print("\nRed tiles: " + str(list(red_moves)))
+                    print("Blue tiles: " + str(list(blue_moves)) + "\n")
 
-    while not game_over:
-        
-        # print game state and get user input
-        while True:             
-            if visual:
-                print_grid(rows, cols, red_moves, blue_moves)
-            else:
-                lst = sorted(all_lengths_red.items(), key=lambda x: (x[0]))
-                print("\nlength distribution of minimal winning paths remaining (Red): ")
-                print(lst)
+                if turn == RED:
+                    print("Red's turn")
+                else:
+                    print("Blue's turn")
                 
-                print("\nRed tiles: " + str(list(red_moves)))
-                print("Blue tiles: " + str(list(blue_moves)) + "\n")
+                print("(1) Place a stone at (x,y)")
+                print("(2) Place a random stone")
+                if turn == RED: 
+                    print("(3) Play the potential strategy")
+                print("(x) Exit")
+
+                opt = input("Choose an option: ").strip("() ")
+                if opt == 'x':
+                    exit(0)
+                elif opt in ['1', '2'] or (opt == '3' and turn == RED):
+                    break
+                else:
+                    print("Option not recognized. Please try again.")
+
+            # let the player choose a tile
+            if opt == '1':
+                while True:
+                    try:
+                        x, y = (int(c.strip("()[] ")) for c in input("Please enter x,y coordinates: ").split(','))
+                        next_move = (x, y)
+                        if is_in_bounds(rows, cols, next_move) and next_move not in red_moves | blue_moves:
+                            break
+                        else:
+                            print("Invalid move. Please try again.")
+                    except:
+                        print("Cannot read input. Please try again.")                  
+
+            # choose a random tile
+            elif opt == '2':
+                open_tiles = get_open_tiles(rows, cols, red_moves, blue_moves)
+                next_move = open_tiles[random.randint(0, len(open_tiles)-1)]
+
+            # play the potential strategy
+            elif opt == '3' and turn == RED:
+
+                # initialize variables
+                open_tiles = get_open_tiles(rows, cols, red_moves, blue_moves)
+                next_move = open_tiles[0]
+                next_move_potential = 0
+                for path in blue_paths:
+                    if next_move not in path:
+                        next_move_potential += (1/2)**(len(path.difference(blue_moves)))
+
+                # find a tile that minimizes the potential function
+                for move in open_tiles[1:]:
+                    move_potential = 0
+                    for path in blue_paths:
+                        if move not in path:
+                            move_potential += (1/2)**(len(path.difference(blue_moves)))
+                    if move_potential < next_move_potential:
+                        next_move = move
+                        next_move_potential = move_potential
 
             if turn == RED:
-                print("Red's turn")
-            else:
-                print("Blue's turn")
-            
-            print("(1) Place a stone at (x,y)")
-            print("(2) Place a random stone")
-            if turn == RED: 
-                print("(3) Play the potential strategy")
-            print("(x) Exit")
+                red_moves.add(next_move)
 
-            opt = input("Choose an option: ").strip("() ")
-            if opt == 'x':
-                exit(0)
-            elif opt in ['1', '2'] or (opt == '3' and turn == RED):
-                break
-            else:
-                print("Option not recognized. Please try again.")
+                # check win condition
+                for path in red_paths:
+                    if path.issubset(red_moves): 
+                        game_over = True
+                        endgame_message = "Red wins!"
+                        if visual: # add color
+                            endgame_message = "\033[0;31m" + endgame_message + "\033[0m"
 
-        # let the player choose a tile
-        if opt == '1':
-            while True:
-                try:
-                    x, y = (int(c.strip("()[] ")) for c in input("Please enter x,y coordinates: ").split(','))
-                    next_move = (x, y)
-                    if is_in_bounds(rows, cols, next_move) and next_move not in red_moves | blue_moves:
-                        break
-                    else:
-                        print("Invalid move. Please try again.")
-                except:
-                    print("Cannot read input. Please try again.")                  
+                # remove minimal winning paths containing new Red tile from Blue's path set
+                new_blue_paths = [p for p in blue_paths if next_move not in p]
+                blue_paths = new_blue_paths
 
-        # choose a random tile
-        elif opt == '2':
-            open_tiles = get_open_tiles(rows, cols, red_moves, blue_moves)
-            next_move = open_tiles[random.randint(0, len(open_tiles)-1)]
-
-        # play the potential strategy
-        elif opt == '3' and turn == RED:
-
-            # initialize variables
-            open_tiles = get_open_tiles(rows, cols, red_moves, blue_moves)
-            next_move = open_tiles[0]
-            next_move_potential = 0
-            for path in blue_paths:
-                if next_move not in path:
-                    next_move_potential += (1/2)**(len(path.difference(blue_moves)))
-
-            # find a tile that minimizes the potential function
-            for move in open_tiles[1:]:
-                move_potential = 0
+                # rebuild path length dictionary
+                all_lengths_blue = {}
                 for path in blue_paths:
-                    if move not in path:
-                        move_potential += (1/2)**(len(path.difference(blue_moves)))
-                if move_potential < next_move_potential:
-                    next_move = move
-                    next_move_potential = move_potential
+                    path_length = len(path)
+                    if path_length in all_lengths_blue:
+                        all_lengths_blue[path_length] += 1
+                    else:
+                        all_lengths_blue[path_length] = 1
 
-        if turn == RED:
-            red_moves.add(next_move)
+            else:
+                blue_moves.add(next_move)
+                
+                # check win condition
+                for path in blue_paths:
+                    if path.issubset(blue_moves):
+                        game_over = True
+                        endgame_message = "Blue wins!"
+                        if visual: # add color
+                            endgame_message = "\033[34m" + endgame_message + "\033[0m"
 
-            # check win condition
-            for path in red_paths:
-                if path.issubset(red_moves): 
-                    game_over = True
-                    endgame_message = "Red wins!"
-                    if visual: # add color
-                        endgame_message = "\033[0;31m" + endgame_message + "\033[0m"
+                # remove minimal winning paths containing new Blue tile from Red's path set
+                new_red_paths = [p for p in red_paths if next_move not in p]
+                red_paths = new_red_paths
 
-            # remove minimal winning paths containing new Red tile from Blue's path set
-            new_blue_paths = [p for p in blue_paths if next_move not in p]
-            blue_paths = new_blue_paths
+                # rebuild path length dictionary
+                all_lengths_red = {}
+                for path in red_paths:
+                    path_length = len(path)
+                    if path_length in all_lengths_red:
+                        all_lengths_red[path_length] += 1
+                    else:
+                        all_lengths_red[path_length] = 1
 
-            # rebuild path length dictionary
-            all_lengths_blue = {}
-            for path in blue_paths:
-                path_length = len(path)
-                if path_length in all_lengths_blue:
-                    all_lengths_blue[path_length] += 1
-                else:
-                    all_lengths_blue[path_length] = 1
+            # if there are no remaining open tiles, the game is a draw
+            if not get_open_tiles(rows, cols, red_moves, blue_moves) and not game_over:
+                game_over = True
+                endgame_message = "It's a draw."
 
+            turn = not turn # end turn
+
+        # print final game state and declare winner
+        if visual:
+            print_grid(rows, cols, red_moves, blue_moves)
         else:
-            blue_moves.add(next_move)
-            
-            # check win condition
-            for path in blue_paths:
-                if path.issubset(blue_moves):
-                    game_over = True
-                    endgame_message = "Blue wins!"
-                    if visual: # add color
-                        endgame_message = "\033[34m" + endgame_message + "\033[0m"
+            lst = sorted(all_lengths_red.items(), key=lambda x: (x[0]))
+            print("\nlength distribution of minimal winning paths remaining (Red): ")
+            print(lst)
 
-            # remove minimal winning paths containing new Blue tile from Red's path set
-            new_red_paths = [p for p in red_paths if next_move not in p]
-            red_paths = new_red_paths
+            print("\nRed tiles: " + str(list(red_moves)))
+            print("Blue tiles: " + str(list(blue_moves)) + "\n")
+        print(endgame_message)
 
-            # rebuild path length dictionary
-            all_lengths_red = {}
-            for path in red_paths:
-                path_length = len(path)
-                if path_length in all_lengths_red:
-                    all_lengths_red[path_length] += 1
-                else:
-                    all_lengths_red[path_length] = 1
-
-        # if there are no remaining open tiles, the game is a draw
-        if not get_open_tiles(rows, cols, red_moves, blue_moves) and not game_over:
-            game_over = True
-            endgame_message = "It's a draw."
-
-        turn = not turn # end turn
-
-    # print final game state and declare winner
-    if visual:
-        print_grid(rows, cols, red_moves, blue_moves)
     else:
-        lst = sorted(all_lengths_red.items(), key=lambda x: (x[0]))
-        print("\nlength distribution of minimal winning paths remaining (Red): ")
-        print(lst)
+        compute_paths(rows, cols, cell_to_avoid, RED)
+        print_result(all_lengths_red, lengths_avoiding_cell_red)
 
-        print("\nRed tiles: " + str(list(red_moves)))
-        print("Blue tiles: " + str(list(blue_moves)) + "\n")
-    print(endgame_message)
-
-else:
-    compute_paths(rows, cols, cell_to_avoid, RED)
-    print_result(all_lengths_red, lengths_avoiding_cell_red)
-
-exit(0)
+    exit(0)
 
 
 
